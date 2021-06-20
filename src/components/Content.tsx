f546d3ec0e8e8892376bdb7eb2eec64a0ca77e3a
cirops <cirops@gmail.com>
2021-06-19 21:21:46 -0300

    completing challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+
+interface ContentProps {
+  movies: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }[],
+  selectedGenreTitle: string;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenreTitle}</span></span>
+      </header>
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
 }
